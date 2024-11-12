aa845024a1111b142387991e67f2e61ead04dcae
ReyhanPA <reyhanputra95@gmail.com>
2024-11-12 23:34:07 +0700

    refactor: separate components

--- src/app/weather/page.js
+++ src/app/weather/page.js
@@ -1,23 +1,12 @@
+import FormSearch from "@/components/formSearch";
+
 export default function WeatherPage() {
   return (
     <div className="flex-col pt-24 bg-white">
-      <form className="flex items-center max-w-sm mx-auto">   
-        <label htmlFor="simple-search" className="sr-only">Search</label>
-        <div className="relative w-full">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"/>
-            </svg>
-          </div>
-          <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-cyan-800 focus:border-cyan-800 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cyan-800 dark:focus:border-cyan-800" placeholder="Search City" required />
-        </div>
-        <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-cyan-800 rounded-lg border border-cyan-800 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-cyan-800 dark:focus:ring-blue-800">
-          <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-          </svg>
-          <span className="sr-only">Search</span>
-        </button>
-      </form>
+      <div className="flex justify-start px-4">
+        <h1 className="text-lg font-semibold text-cyan-800">Weather</h1>
+      </div>
+      <FormSearch />
       <h1 className="text-2xl font-bold text-center mt-4 text-cyan-800">Not implemented yet</h1>
     </div>
   );
