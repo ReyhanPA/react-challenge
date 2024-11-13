eceeb37888bddb14451ea3491da7a3b2f8ed1105
ReyhanPA <reyhanputra95@gmail.com>
2024-11-14 00:20:41 +0700

    feat: add error and loading state weather page

--- src/app/weather/page.js
+++ src/app/weather/page.js
@@ -4,27 +4,25 @@ import { useState } from "react";
 import FormSearch from "@/components/formSearch";
 import WeatherCard from "@/components/weatherCard";
 import { getCityLatLon, getWeatherData } from "@/utils/apiService";
+import ClipLoader from "react-spinners/ClipLoader";
 
 export default function WeatherPage() {
   const [dataAllWeather, setDataAllWeather] = useState([]);
   const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleGetWeather = async (city) => {
     try {
+      setLoading(true);
       const dataLatLon = await getCityLatLon(city);
       const allWeather = [];
       if (dataLatLon) {
         const data = await getWeatherData(dataLatLon.lat, dataLatLon.lon);
         const weather = data.list;
         weather.map((item) => {
-          const date = new Date(item.dt * 1000);
-          const formattedDate = date.toLocaleDateString('id-ID');
-          const formattedTime = date.toLocaleTimeString('id-ID', {
-            hour12: false,
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-          });
+          const dateTime = item.dt_txt;
+          const [formattedDate, formattedTime] = dateTime.split(" ");
+
   
           const main = item.weather[0].main;
           const description = item.weather[0].description;
@@ -33,13 +31,18 @@ export default function WeatherPage() {
           allWeather.push({ date: formattedDate, time: formattedTime, main, description, icon });
         });
         setDataAllWeather(allWeather);
+        console.log(dataAllWeather);
         const capitalizedCity = city.charAt(0).toUpperCase() + city.slice(1).toLowerCase();
         setCity(capitalizedCity);
       } else {
         console.log("City not found or error occurred.");
+        setCity("City not found");
+        setDataAllWeather([]);
       }
     } catch (error) {
       console.log("Error fetching city data:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -50,10 +53,10 @@ export default function WeatherPage() {
       </div>
       <FormSearch onSearch={handleGetWeather} />
       <div className="flex justify-center px-4 my-4">
-        <h1 className="text-base font-semibold text-cyan-800">{city}</h1>
+        <h1 className="text-base font-semibold text-cyan-800">{loading ? "" : city}</h1>
       </div>
       <div className="flex gap-2 flex-wrap justify-center items-center px-4">
-        {dataAllWeather.map((item, index) => (
+        {loading ? <ClipLoader color="#36d7b7" /> : dataAllWeather.map((item, index) => (
           <WeatherCard
             key={index}
             date={item.date}
--- src/components/formSearch.js
+++ src/components/formSearch.js
@@ -27,7 +27,7 @@ export default function FormSearch({ onSearch }) {
           value={city}
           onChange={(e) => setCity(e.target.value)}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-cyan-800 focus:border-cyan-800 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cyan-800 dark:focus:border-cyan-800"
-          placeholder="Search City"
+          placeholder="Search City (Indonesia)"
           required
         />
       </div>
